18508c61701f52ac7e2bd1ad7b9f7ce77522bade
Marek Vargovčík <vargovcik.marek@gmail.com>
2019-12-03 17:26:54 +0000

    fixed css/js issues, added comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,22 +2,36 @@ import '../styles/index.scss';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+// Feather svg icons init
 feather.replace();
+
+// Bootstrap select init
 $('select').selectpicker({
   style: '',
   selectedTextFormat: 'count',
   countSelectedText: count => `${count} selected`
 });
+
+// Toasts init
 $('.toast')
   .toast({
     autohide: false
   })
   .toast('show');
 
-$('.form-control[type=date], .form-control[type=datetime]').flatpickr({
-  minDate: 'today',
+// Flatpickr init
+$('.form-control[type=date]').flatpickr({
   dateFormat: 'd.m.Y'
 });
+
+// Flatpickr init
+$('.form-control[type=datetime]').flatpickr({
+  dateFormat: 'd.m.Y H:i',
+  enableTime: true,
+  time_24hr: true
+});
+
+// Carousel init
 $('[data-slider]').slick({
   dots: true,
   infinity: true,
@@ -37,5 +51,8 @@ $('[data-slider]').slick({
     }
   ]
 });
+// Show slider after carousel init
 $('[data-slider]').show();
+
+// Fancy lightbox init
 $('[data-slider-image]').fancybox();
